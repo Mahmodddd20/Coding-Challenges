0c1c6bfe3ca64e5683c30a5bbcadf627af9015bd
mahmodddd20 <mahmodddd20@hotmail.com>
2021-05-17 09:58:40 +0300

    add 5 6 7 8 questions

---
+++ question5.js
@@ -0,0 +1,6 @@
+const findCommonLettersInWords = (words) => {
+    // do your magic here..
+}
+
+console.log( findCommonLettersInWords(["Saleem", "Soda", "Khaled"]) ); // output: ['a']
+console.log( findCommonLettersInWords(["Pepsi", "Kitkat", "Oreo"]) ); // output: []
---
+++ question6.js
@@ -0,0 +1,5 @@
+const averageBooks = (numberOfBoxes, stages) => {
+    // do your magic here..
+}
+
+console.log( averageBooks(3, [[1, 2, 3], [1, 3, 5]]) ); // output: 7
