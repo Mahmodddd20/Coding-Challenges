c05fef78d5280406b6b60b39a29134c40d902fc1
mahmodddd20 <mahmodddd20@hotmail.com>
2021-05-20 11:22:15 +0300

    add question 5

--- question5.js
+++ question5.js
@@ -1,6 +1,28 @@
 const findCommonLettersInWords = (words) => {
-    // do your magic here..
+    let CommonLetter = [];
+    for (let indexFirstWord = 0; indexFirstWord < words[0].length; indexFirstWord++) {
+        for (let indexRestWords = 1; indexRestWords < words.length; indexRestWords++) {
+            if (!words[indexRestWords].includes(words[0][indexFirstWord])) {
+                break
+            }
+            CommonLetter.push(words[0][indexFirstWord]);
+        }
+    }
+    if (CommonLetter.length < words.length - 1) {
+        CommonLetter = ['']
+    } else {
+        for (let k = 0; k < CommonLetter.length-1; k++) {
+            if(CommonLetter[k] == CommonLetter[k+1]){
+                return CommonLetter=[CommonLetter[k]]
+            }
+        }
+    }
+
+
+    return CommonLetter;
 }
 
-console.log( findCommonLettersInWords(["Saleem", "Soda", "Khaled"]) ); // output: ['a']
-console.log( findCommonLettersInWords(["Pepsi", "Kitkat", "Oreo"]) ); // output: []
+
+console.log(findCommonLettersInWords(["Saleem", "Soda", "Khaled"])); // output: ['a']
+console.log(findCommonLettersInWords(["Pepsi", "Kitkat", "Oreo"])); // output: []
+console.log(findCommonLettersInWords(["Slaleem", "Solda", "Khaled","ahmad"])); // output: ['a']
