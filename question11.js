2c4ddf4158121a91cd5dfdbce41cfe1e001ac64e
mahmodddd20 <mahmodddd20@hotmail.com>
2021-06-08 15:47:51 +0300

    question 11

--- question11.js
+++ question11.js
@@ -2,22 +2,24 @@ function fullPath(path) {
     let startingPoints = [];
     let endingPoints = [];
     let result = [];
+
     for (let i = 0; i < path.length; i++) {
         startingPoints.push(path[i].from);
         endingPoints.push(path[i].to);
     }
+
     for (let i = 0; i < path.length; i++) {
         if (!endingPoints.includes(path[i].from)) {
             result.push(path[i].from);
             break;
         }
     }
+
     for (let i = 0; i < path.length; i++) {
         if (startingPoints.includes(result[i])) {
             let a = startingPoints.indexOf(result[i]);
             result.push(endingPoints[a]);
         }
-
     }
     return result;
 
